5055abe96a51dc8e9dd0f233d46acc33bab5772f
Marcus Siegel <msiegel37@gmail.com>
2020-08-17 19:57:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,9 +20,9 @@ def consolidate_cart(cart)
   new_cart = []
   counter = 0 
   while counter < cart.length 
-  find_item_by_name_in_collection(cart, collection) 
-   while counter < collection.length 
-   if cart[counter] == collection
+  new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart) 
+   if new_cart_item != nil 
+     new_cart_item[:count] += 1 
   end 
   counter += 1 
  end 
